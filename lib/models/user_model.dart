4a2d50138a87d598c6a8097ea426e37730d3c5d8
Adi <adidwipm@gmial.com>
2026-03-11 15:18:29 +0700

    aw

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -6,7 +6,5 @@ class User {
 }
 
 User user1 = User(username: '170', password: '170');
-User user2 = User(username: 'owi', password: '170');
-User user3 = User(username: 'wowo', password: '170');
 
-List<User> userList = [user1, user2, user3];
+List<User> userList = [user1];
